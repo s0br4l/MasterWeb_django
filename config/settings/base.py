ef041fc4209a35d28282c0053d5f7e74e1317872
lsobral <lucas.sobral5@gmail.com>
2022-08-30 00:13:09 -0300

    teste base

--- config/settings/base.py
+++ config/settings/base.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = 'static/'
 
